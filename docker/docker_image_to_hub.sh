9b406b29e02b1f8b6eb29b8e1a37881c8b192298
Stefan van der Walt <stefanv@berkeley.edu>
2016-12-21 23:53:57 -0800

    Add warning not to run docker image upload script

--- docker/docker_image_to_hub.sh
+++ docker/docker_image_to_hub.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+echo "Note: this script should probably no longer be run, given that"
+echo "      builds are now automated on hub.docker.com"
+echo
+echo "Press any key to continue anyway, or Ctrl-C to quit"
+
+read
+
 docker build -t elegantscipy/elegantscipy .
 docker login
 docker push elegantscipy/elegantscipy
