2dc17bb73399723a1a2d91fc525a12c32306c51f
Stefan van der Walt <stefanv@berkeley.edu>
2017-01-31 08:48:49 -0800

    WIP: mathml improvements

--- tools/latex_to_mathml.py
+++ tools/latex_to_mathml.py
@@ -7,6 +7,7 @@ Translate dollar expressions inside HTML files to MathML
 import sys
 import os
 import re
+import html
 
 import subprocess
 from subprocess import PIPE
@@ -21,20 +22,27 @@ html_file = sys.argv[1]
 html_dir = os.path.abspath(os.path.dirname(html_file))
 
 with open(html_file, 'rb') as f:
-    data = f.read()
+    data = f.read().decode('utf-8')
+
+# Temporary workaround for figure captions containing math.  Destroy them.
+data = re.sub('alt=\'.*?\'', '', data, flags=re.DOTALL)
 
 def math_wrap(matchobj):
+    math = matchobj.group(1)
+#    math = html.escape(math)
+
     return '''
 <div xmlns:b="http://gva.noekeon.org/blahtexml" b:inline="{}"/>
-'''.format(matchobj.group(1))
+'''.format(math)
 
-html_with_hooks = re.sub('\$(.*?)\$', math_wrap, data.decode('utf-8'), flags=re.MULTILINE)
+html_with_hooks = re.sub('\$(.*?)\$', math_wrap, data, flags=re.DOTALL)
 
 ps = subprocess.Popen(('blahtexml', '--xmlin'), stdin=PIPE, stdout=PIPE, stderr=PIPE)
 stdout_data, stderr_data = ps.communicate(input=html_with_hooks.encode('utf-8'))
 
 if ps.returncode != 0:
-    print(stderr_data.decode('utf-8'))
+    sys.stderr.write(stderr_data.decode('utf-8'))
+    sys.stderr.write(stdout_data.decode('utf-8'))
     sys.exit(-1)
 
 
@@ -45,6 +53,6 @@ def inner_only(matchobj):
 
 post_mathml = re.sub(
     '<div xmlns:b="http://gva.noekeon.org/blahtexml">(.*?)</div>',
-    inner_only, post_mathml, flags=re.MULTILINE)
+    inner_only, post_mathml, flags=re.DOTALL)
 
 print(post_mathml.replace('<?xml version="1.0" encoding="UTF-8"?>', ''))
