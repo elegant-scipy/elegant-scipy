6bffef588d1e4e4e9622b5dd0ecfbcc86ec278a3
Stefan van der Walt <stefanv@berkeley.edu>
2017-04-24 10:02:35 -0700

    Use a language with sane unicode handling

--- tools/caption_crunch.py
+++ tools/caption_crunch.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
 Insert captions around output figures
--- tools/wrap_callouts.py
+++ tools/wrap_callouts.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
 Insert HTMLBook tags where needed.
