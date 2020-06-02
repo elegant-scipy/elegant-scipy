cc931613d3561cdacfc630af33646514c462a9c1
Ross Barnowski <rossbar@berkeley.edu>
2020-06-02 15:47:08 -0700

    Update conf.py to use sphinx-book-theme

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -42,9 +42,9 @@ exclude_patterns = ['_build', 'Thumbs.db', 'README.md']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
 # NOTE: For testing with jupyter-book theme sphinx ext
-#html_theme = 'sphinx_jupyter_book_theme'
+html_theme = 'sphinx_book_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
